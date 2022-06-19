5949c02858f64e0ae713f151427479e28c3e3728
Dima Smirnov <dim.smir@inbox.ru>
2022-06-19 14:27:14 +0600

    add react custom hooks recipes

---
+++ React/Hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { Region } from "@utils/common";
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: type, delay: number): { debounceValue: type; isDebouncing: boolean } => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  const [isDebouncing, setDebouncing] = useState(false);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+      setDebouncing(false);
+    }, delay);
+    return () => {
+      setDebouncing(true);
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return { debouncedValue, isDebouncing };
+};
