67f0918f387b2adf8ad908dadfc3c0d8c54d73e7
Dima Smirnov <dim.smir@inbox.ru>
2023-02-03 13:24:25 +0600

    add new hook

---
+++ React/Hooks/useBooleanState.tsx
@@ -0,0 +1,21 @@
+import {useState, useCallback} from 'react'
+
+const useBooleanState = (initialValue = false) => {
+  const [value, setValue] = useState(initialValue)
+
+  const setTrueValue = useCallback(()=>{
+    setValue(true)
+  }, [])
+
+    const setFalseValue = useCallback(()=>{
+    setValue(false)
+  }, [])
+
+    const toggleValue = useCallback(()=>{
+    setValue(!value)
+  }, [])
+
+  return [value, setTrueValue, setFalseValue, toggleValue] as const
+}
+
+export default useBooleanState
